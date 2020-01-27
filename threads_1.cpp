942ebf0eaa7688870259530d84407145fa2f58aa
ajay <ajay.gunalan@iit.it>
2020-01-27 11:46:15 +0100

    join example 1

--- threads_1.cpp
+++ threads_1.cpp
@@ -6,10 +6,13 @@ using namespace std;
 void run(int count) {
 	while(count --> 0)
 		cout << "Inside run" <<endl;
+	std::this_thread::sleep_for(chrono::seconds(3));
 }
 
 int main() {
 	std::thread t1(run, 10);
+	cout << "Before Join \n" << endl;
 	t1.join();
+	cout << "After Join  \n" << endl;
 	return 0;
 }
